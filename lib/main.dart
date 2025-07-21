0138f1c94a76cfde8bb2d84a707a273615a1788a
azzamt11 <abdullahazzamt11@gmail.com>
2025-07-21 14:49:47 +0700

    fixing

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:bloc_app_template/common/app.dart';
 import 'package:bloc_app_template/utilities/injector.dart';
-import 'package:stetho_network_inspector/stetho_network_inspector.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Stetho.initialize();
+  //await Stetho.initialize();
 
   await SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
